a40db7af8d7f7a1211b767d2b758fcd6519fb724
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2024-02-13 18:26:16 +0200

    Update numby.py

--- codes/numby.py
+++ codes/numby.py
@@ -12,6 +12,7 @@ def show(item):
     print("type: " + str(item.dtype))
     print("dimentions: " + str(item.ndim))
     print("shape: " + str(item.shape))
+    print("size: " + str(item.size))
 
 x = np.array(12)
 y = np.array([[12 , 3 , 6 , 14] , [2 , 4 , 3 , 5] ] )
@@ -22,8 +23,37 @@ show(y)
 x = x.astype(float)
 
 
+def basic_tools():
+    # slicing
+    x[index] # Access elements of the array by index.
+    x[start:end:step] # Slice the array to extract a subset of elements.
+    x[:, col_index] # Access a specific column of a 2D array.
+    x[row_index, :] # Access a specific row of a 2D array.
 
+    # Define a boolean mask
+    mask = arr % 2 == 0  # Select even numbers
+    x[mask] # Use a boolean mask to filter elements of the array.
 
+    # other methods 
+    np.array() # Create an array from a Python list or tuple.
+    np.arange() # Create an array with evenly spaced values within a given interval.
+    np.zeros() # Create an array filled with zeros.
+    np.ones() # Create an array filled with ones.
+    np.random.rand() # Create an array with random values from a uniform distribution.
+    np.random.randint() # Generate random integers.
+    np.random.randn() # Generate random numbers from a standard normal distribution.
+    np.random.choice() # Generate a random sample from a given 1D array.
+
+    np.sum() # Sum of array elements.
+    np.mean() # Mean of array elements.
+    np.std() # Standard deviation of array elements.
+    np.min() # Minimum value in the array.
+    np.max() # Maximum value in the array.
+    np.dot() # Dot product of two arrays.
+
+    x.reshape() # Reshape the array into a new shape.
+    x.flatten() #  Flatten the array into a 1D array.
+    np.concatenate() # Concatenate arrays along a specified axis.
 
 # temp codes 
 # for name, value in globals().items():
